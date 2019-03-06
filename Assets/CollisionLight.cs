aa1ef2e8f474e934e0476afb5727b53aa1ed83c5
Jonathan Ganz <j_ganz@cda124ev5635w.cdaad.concordia.ca>
2019-03-06 11:56:33 -0500

    Lightnodes
    Added lightnode and working script for illumination upon collision. Make sure each LightNode is called such specifically, with no spaces

---
+++ Assets/CollisionLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CollisionLight: MonoBehaviour
+{
+    public GameObject Ball = GameObject.Find("Ball");
+    public float speed;
+    public GameObject Light;
+    public Light lightComp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "LightNode")
+        {
+            lightComp = col.collider.gameObject.GetComponent<Light>();
+            lightComp.range = 20;
+            lightComp.intensity = 5;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
